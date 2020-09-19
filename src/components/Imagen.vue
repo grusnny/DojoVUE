09eea6dbf4e1b38065e910fcd788bbda0dc20582
grusnny <37426267+grusnny@users.noreply.github.com>
2020-09-19 10:06:12 -0500

    Update Imagen.vue

--- src/components/Imagen.vue
+++ src/components/Imagen.vue
@@ -2,7 +2,7 @@
     <v-row>
         <v-col v-for="item in items" v-bind:key="item.id" sm="4" cols="12">
             <v-card>
-                <img :src="item.hits" v-bind="item.">
+                <!-- <img :src="item.hits" v-bind="item."> -->
                 <v-card-title>{(item.user)}</v-card-title>
             </v-card>
         </v-col>
